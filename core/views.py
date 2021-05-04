98d82e144b8fd1ae84ef66288b7bad0f8e5ec7b7
WaFee <Herobrin504@gmail.com>
2021-05-04 23:18:35 +0700

    Add add-memory page
    Added add-memory page with map, js script for map and form with autofill opportunities. Updated urls, views and style files.

--- core/views.py
+++ core/views.py
@@ -3,9 +3,14 @@ from django.shortcuts import render, redirect
 
 
 def home(request):
+    print(request.user.id)
     return render(request, "core/home.html")
 
 
 def logout_view(request):
     logout(request)
     return redirect('home-page')
+
+
+def add_memory(request):
+    return render(request, "core/add-memory.html")
