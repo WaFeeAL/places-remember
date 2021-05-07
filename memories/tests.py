9e0b08e9b056f5055a900966afc826c76f65db1d
WaFee <Herobrin504@gmail.com>
2021-05-07 15:40:29 +0700

    Add tests for MemoryModel
    Add tests for add_memory and get_memory_list. Update views for MemoryModel.

--- memories/tests.py
+++ memories/tests.py
@@ -1,3 +1,39 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+from .models import MemoryModel
+from .serializers import MemorySerializer
+from .views import get_memory_list, post_memory
+
+User = get_user_model()
+
+
+class MemoryModelTestCases(TestCase):
+
+    def setUp(self) -> None:
+        self.user = User.objects.create(username='testuser', password='password')
+
+    def test_add_memory(self):
+        factory = RequestFactory()
+        request = factory.get('')
+        request.user = self.user
+        request.method = 'POST'
+        request.POST = {'user_id': 444,
+                        'location_name': 'Красноярск',
+                        'location_address': 'Красноярск',
+                        'location_memories': 'Красноярск'}
+        response = post_memory(request)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/home')
+
+    def test_get_memory_list(self):
+        factory = RequestFactory()
+        request = factory.get('')
+        request.user = self.user
+        request.method = 'GET'
+
+        friends = MemoryModel.objects.filter(user_id=request.user.id).all()
+        serializer = MemorySerializer(friends, many=True)
+
+        response = get_memory_list(request)
+        self.assertEqual(response, serializer.data)
--- memories/urls.py
+++ memories/urls.py
@@ -4,9 +4,10 @@ from . import views
 
 urlpatterns = [
     # path('friend/', FriendList.as_view()),
-    path('', views.memory_list, name='memory-list'),
+    path('get-memory-list', views.get_memory_list, name='get-memory-list'),
     path('delete-memory/<int:memory_id>', views.delete_memory, name='delete-memory'),
-    path('<int:memory_id>', views.memory_detail, name='memory-detail'),
+    path('get-memory/<int:memory_id>', views.get_memory, name='get-memory'),
+    path('post-memory', views.post_memory, name='post-memory'),
     # path('add/<int:product_id>', views.cart_add, name='cart-add'),
     # path('remove/<int:product_id>', views.cart_remove, name='cart-remove'),
 ]
