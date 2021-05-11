9a17014cac816a0be23e1849692384672ddc0b18
WaFee <Herobrin504@gmail.com>
2021-05-11 20:43:17 +0700

    Add staticfiles

--- places_remember/settings.py
+++ places_remember/settings.py
@@ -168,13 +168,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'core/static'),
 )
 
 # Default primary key field type
